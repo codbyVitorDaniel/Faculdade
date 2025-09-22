caf4063812ae5b3b36a1d2a9bf6d12a1c143af01
vitordanielrosa <143666152+vitordanielrosa@users.noreply.github.com>
2025-09-22 19:59:12 -0300

    exemplo de let e console.log

---
+++ aula de javascript/aularevisao/exemplo2.js
@@ -0,0 +1,13 @@
+let nota = 7;
+
+if (nota >= 6){
+    console.log("Aprovado")
+} else{
+    console.log("Reprovado")
+}
+
+console.log(nota == "7")
+console.log(nota === "7")
+console.log(nota != "5")
+console.log(nota >= "7")
+
