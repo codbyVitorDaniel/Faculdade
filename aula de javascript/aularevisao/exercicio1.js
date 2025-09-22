efbb7a7dcdbf4135ec34d82eab3cd4d988e3af66
vitordanielrosa <143666152+vitordanielrosa@users.noreply.github.com>
2025-09-22 19:59:44 -0300

    exemplo de objeto e dicionario

--- aula de javascript/aularevisao/exercicio1.js
+++ aula de javascript/aularevisao/exercicio1.js
@@ -2,3 +2,10 @@ let nome = "Vitor"
 const idade = 20;
 let ativo = true;
 let frutas = ["Maça", "Banana", "Uva", ]
+let alunos = {nome: "Carlos" , idade:90}
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof ativo);
+console.log(typeof frutas);
+console.log(typeof alunos);
