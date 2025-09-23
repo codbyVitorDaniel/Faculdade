9a1372831145f73c87c9c34315972a3574892813
vitordanielrosa <143666152+vitordanielrosa@users.noreply.github.com>
2025-09-22 22:00:56 -0300

    doom

---
+++ aula de javascript/aularevisao/exemplo5.js
@@ -0,0 +1,4 @@
+let titulo = document.getElementById("titulo")
+let itens = document.getElementsByTagName("li")
+let lista = document.querySelector(".lista")
+let todos = document.querySelectorAll(".lista li")
