53471ec37f2505579b03e72e2a209a5c54fabad0
vitordanielrosa <143666152+vitordanielrosa@users.noreply.github.com>
2025-09-22 21:22:01 -0300

    exemplo2

--- aula de javascript/aularevisao/exemplo2.js
+++ aula de javascript/aularevisao/exemplo2.js
@@ -11,3 +11,19 @@ console.log(nota === "7")
 console.log(nota != "5")
 console.log(nota >= "7")
 
+
+// let situaçao = nota >= 5.5 ? "Aprovado" : "Reprovado"
+// console.log(situaçao)
+
+let fruta = "Manga"
+switch (nota) {
+    case nota >= 6:
+        console.log("Aprovado")
+        break;
+    case nota <= 6 && nota >= 4:
+        console.log("Esta em recuperaçao")
+        break;
+    case nota < 4:
+        console.log("Reprovado")
+        break
+}
