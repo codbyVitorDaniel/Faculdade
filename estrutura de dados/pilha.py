56f1956e89e31329c2cb7af82ed98c6c1758effd
vitordanielrosa <143666152+vitordanielrosa@users.noreply.github.com>
2025-09-23 19:56:59 -0300

    Exercicio de pilha

---
+++ estrutura de dados/pilha.py
@@ -0,0 +1,13 @@
+from stack import Stack
+
+MinhaPilha = Stack()
+
+MinhaPilha.push("Universidade de Vassouras - Campus Vassouras")
+MinhaPilha.push("Universidade de Vassouras - Campus Marica")
+MinhaPilha.push("Universidade de Vassouras - Campus Saquarema")
+
+print(MinhaPilha.items)
+
+MinhaPilha.pop()
+
+print(MinhaPilha.items)
