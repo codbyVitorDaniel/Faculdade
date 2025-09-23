beab40f26adb9c7d19d59e60ec8cabb632ac1758
vitordanielrosa <143666152+vitordanielrosa@users.noreply.github.com>
2025-09-22 22:06:41 -0300

    feito

--- aula de javascript/aularevisao/exemplo5.js
+++ aula de javascript/aularevisao/exemplo5.js
@@ -2,3 +2,22 @@ let titulo = document.getElementById("titulo")
 let itens = document.getElementsByTagName("li")
 let lista = document.querySelector(".lista")
 let todos = document.querySelectorAll(".lista li")
+
+console.log(titulo.innerText);
+console.log(itens.length);
+console.log(lista.innerHTML);
+console.log(todos)
+
+
+
+// manipulaçoes
+titulo.innerText = "Adicionei um Novo Titulo"
+lista.append(" + um texto")
+
+let botao = document.getElementById("btn")
+let resultado = document.getElementById("resultado")
+
+botao.addEventListener("click", () =>
+{
+    resultado.innerHTML = "<b> Voce clicou no boato<b>"
+})
