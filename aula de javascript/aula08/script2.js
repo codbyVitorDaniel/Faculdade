1ce42c0cfb840b8f7b85610a893df45d4d163bf1
codbyVitorDaniel <143666152+codbyVitorDaniel@users.noreply.github.com>
2025-11-03 21:21:45 -0300

    adicionado o poo

--- aula de javascript/aula08/script2.js
+++ aula de javascript/aula08/script2.js
@@ -1,12 +1,24 @@
-class Person {
+class Person1 {
+    static hands = 2;
+
+    age = 0;
     constructor(name) {
         this.name = name
     }
+    sayHi(){
+        console.log(`${this.name} esta dizendo?`)
+    }
 }
 
-class Student extends Person{
+class Student extends Person1{
     constructor(name, id) {
         super(name)
         this.id = id
     }
 }
+
+pessoa1 = new Student('Joaquim')
+pessoa1 = new Student('OI')
+pessoa1 = new Student('GOl')
+pessoa1.age = 20
+console.log(`Meus personagens tem: ${pessoa1.hands}`)
